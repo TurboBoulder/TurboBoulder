ab30cd73554280aaa8de8f335997c46a92d1c3d8
Jim Groth <jim.groth@gmail.com>
2023-07-15 17:24:02 +0200

    Create IEmailService.cs

---
+++ api/Services/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace IdaWebApplicationTemplate.Services
+{
+    public interface IEmailService
+    {
+        public Task<bool> SendEmailAsync(string title, string content, string recipient);
+    }
+}
