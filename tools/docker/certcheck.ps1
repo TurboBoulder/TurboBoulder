c95d69041e3b6cd283fced5e7e5346b1d3208b34
Jim Groth <jim.groth@gmail.com>
2023-07-20 22:20:03 +0200

    removed old files

--- tools/docker/certcheck.ps1
+++
@@ -1,5 +0,0 @@
-$webRequest = [Net.HttpWebRequest]::Create("https://localhost:7056")
-$webRequest.GetResponse() | Out-Null
-$cert = $webRequest.ServicePoint.Certificate
-$bytes = $cert.Export([Security.Cryptography.X509Certificates.X509ContentType]::Cert)
-set-content -value $bytes -encoding byte -path "serverCert.cer"
