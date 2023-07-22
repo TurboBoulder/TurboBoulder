2cfac240bfca91c20757b7e5197a23100623011c
Jim Groth <jim.groth@gmail.com>
2023-07-22 18:37:23 +0200

    Removed non needed comments

--- tools/cli/Program.cs
+++ tools/cli/Program.cs
@@ -7,16 +7,3 @@ using WebTemplateCLI;
 await Setup.Run();
 
 
-//// Get the host name of the current machine
-//string hostName = Dns.GetHostName();
-
-//// Get the IP addresses associated with the host name
-//IPAddress[] addresses = Dns.GetHostAddresses(hostName);
-
-//foreach (var item in addresses)
-//{
-//	if (item.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
-//	{
-//		Console.WriteLine(item.ToString()); 
-//	}
-//}
